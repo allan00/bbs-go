7fe14b24c224986deb3cf660ce4800b533125495
qiuping.wu <qiuping.wu@shopee.com>
2022-10-11 23:16:21 +0800

    update config

--- site/nuxt.config.js
+++ site/nuxt.config.js
@@ -87,7 +87,7 @@ export default {
   },
 
   proxy: {
-    '/api/': 'http://114.132.156.133:5678',
+    '/api/': 'http://127.0.0.1:8082',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
